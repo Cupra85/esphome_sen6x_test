2bbbc0fcc85203710bcf8954a17ccd95c4c30378
Cupra85 <daniel.curten@gmail.com>
2025-11-16 13:20:25 +0100

    Refactor imports and add header inclusion

--- components/sen6x/__init__.py
+++ components/sen6x/__init__.py
@@ -1,14 +1,19 @@
-from esphome.components import automation
-from esphome.const import CONF_ID
 import esphome.codegen as cg
+from esphome.components import automation
 
+# Namespace & Klassen müssen exakt zu deinem C++ passen:
 sen6x_ns = cg.esphome_ns.namespace("sen6x")
 SEN5XComponent = sen6x_ns.class_("SEN5XComponent", cg.PollingComponent, cg.I2CDevice)
 
+# Action-Klassen referenzieren
 StartFanAction = sen6x_ns.class_("StartFanAction", automation.Action)
 StopMeasurementAction = sen6x_ns.class_("StopMeasurementAction", automation.Action)
 StartMeasurementAction = sen6x_ns.class_("StartMeasurementAction", automation.Action)
 
+# Header EINBINDEN, damit der Codegenerator die Klassen kennt:
+cg.add(cg.include("sen6x/automation.h"))
+
+# Actions registrieren – die Namen müssen exakt so ins YAML:
 automation.register_action(
     "sen6x.start_fan_cleaning", StartFanAction, automation.maybe_simple_id(SEN5XComponent)
 )
@@ -18,3 +23,4 @@ automation.register_action(
 automation.register_action(
     "sen6x.start_measurement", StartMeasurementAction, automation.maybe_simple_id(SEN5XComponent)
 )
+
