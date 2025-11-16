9283498bf6bc1484578e2c8a32bdc705d6de685e
Cupra85 <daniel.curten@gmail.com>
2025-11-16 13:13:10 +0100

    Clean up comments in automation.h
    Removed comments for actions in automation.h

--- components/sen6x/automation.h
+++ components/sen6x/automation.h
@@ -7,7 +7,6 @@
 namespace esphome {
 namespace sen6x {
 
-// --- Fan Cleaning ---
 template<typename... Ts> class StartFanAction : public Action<Ts...> {
  public:
   explicit StartFanAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
@@ -16,7 +15,6 @@ template<typename... Ts> class StartFanAction : public Action<Ts...> {
   SEN5XComponent *sen6x_;
 };
 
-// --- Stop Measurement ---
 template<typename... Ts> class StopMeasurementAction : public Action<Ts...> {
  public:
   explicit StopMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
@@ -25,7 +23,6 @@ template<typename... Ts> class StopMeasurementAction : public Action<Ts...> {
   SEN5XComponent *sen6x_;
 };
 
-// --- Start Measurement ---
 template<typename... Ts> class StartMeasurementAction : public Action<Ts...> {
  public:
   explicit StartMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
