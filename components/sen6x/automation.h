bee851b7f10f174d537c71bba7ec94d0581f1e85
Cupra85 <daniel.curten@gmail.com>
2025-11-16 13:02:44 +0100

    Add Start and Stop Measurement Actions for SEN5X

--- components/sen6x/automation.h
+++ components/sen6x/automation.h
@@ -7,6 +7,24 @@
 namespace esphome {
 namespace sen6x {
 
+template<typename... Ts> class StartMeasurementAction : public Action<Ts...> {
+ public:
+  explicit StartMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
+  void play(Ts... x) override { this->sen6x_->start_measurement(); }
+
+ protected:
+  SEN5XComponent *sen6x_;
+};
+
+template<typename... Ts> class StopMeasurementAction : public Action<Ts...> {
+ public:
+  explicit StopMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
+  void play(Ts... x) override { this->sen6x_->stop_measurement(); }
+
+ protected:
+  SEN5XComponent *sen6x_;
+};
+
 template<typename... Ts> class StartFanAction : public Action<Ts...> {
  public:
   explicit StartFanAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
