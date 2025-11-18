83e99f7a959421670fec3d3e8d88ee5181fb5def
Cupra85 <daniel.curten@gmail.com>
2025-11-18 16:04:42 +0100

    Update sen6x.h

--- components/sen6x/sen6x.h
+++ components/sen6x/sen6x.h
@@ -100,8 +100,10 @@ class SEN5XComponent : public PollingComponent, public sensirion_common::Sensiri
   bool start_measurement();
   bool stop_measurement();
   bool start_fan_cleaning();
+    // --- SEN6x getters (4.8.40 / 4.8.42) ---
   bool read_ambient_pressure(uint16_t &pressure_hpa);
   bool read_sensor_altitude(uint16_t &altitude_m);
+
 
   std::string get_product_name() const { return product_name_; }
   uint16_t get_firmware_version() const { return firmware_version_; }
